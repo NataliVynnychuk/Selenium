40efc0ab6c42cc41d39d4d4bfddaf7492e17721e
Nataliya Vynnychuk <nataliromanova010@gmail.com>
2022-02-23 10:05:40 +0200

    Selenium

--- src/main/java/MobileTest.java
+++ src/main/java/MobileTest.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -27,7 +28,6 @@ public class MobileTest {
         By lastName = By.xpath("//input[@data-qa-node='lastNamedebitSource']");
         By buttonSubmitToTheCart = By.xpath("//button[@data-qa-node='submit']");
 
-
         // 3. Действие над элементом
         driver.findElement(phoneNumber).sendKeys("630734074");
         driver.findElement(amount).sendKeys("500");
@@ -37,6 +37,29 @@ public class MobileTest {
         driver.findElement(firstName).sendKeys("Peter");
         driver.findElement(lastName).sendKeys("Scroggs");
         driver.findElement(buttonSubmitToTheCart).click();
+
+        /**
+         * Проверка теста
+         * */
+
+        //System.out.println(driver.findElement(By.xpath("//div[@data-qa-node='amount']")).getText());
+
+        By operator = By.xpath("//span[@data-qa-node='nameB']");
+        By numberCard = By.xpath("//td[@data-qa-node='card']");
+        By replenishmentAmount = By.xpath("//div[@data-qa-node='amount']");
+        By commissionCurrency = By.xpath("//span[@data-qa-node='commission-currency']");
+        By commissionAmount = By.xpath("//span[@data-qa-node='commission']");
+        By purposePayment = By.xpath("//div[@data-qa-node='category']");
+        By paymentDescription = By.xpath("//div[@data-qa-node='details']");
+
+
+        Assert.assertEquals("Lifecell Ukraine", driver.findElement(operator).getText());
+        Assert.assertEquals("4506 **** **** 4797", driver.findElement(numberCard).getText());
+        Assert.assertEquals("500 UAH", driver.findElement(replenishmentAmount).getText());
+        Assert.assertEquals(" UAH", driver.findElement(commissionCurrency).getText());
+        Assert.assertEquals("4", driver.findElement(commissionAmount).getText());
+        Assert.assertEquals("Поповнення мобільного", driver.findElement(purposePayment).getText());
+        Assert.assertEquals("Поповнення телефону. На номер +380630734074", driver.findElement(paymentDescription).getText());
 
     }
 
